65847c928810f9848ad29968f0a064fcabd04dd8
wahyudwiprstka <wahyudwiprstka@gmail.com>
2024-08-16 17:11:51 +0800

    test commit

--- app/admin/satuan/page.tsx
+++ app/admin/satuan/page.tsx
@@ -5,6 +5,7 @@ import { columns } from "./columns";
 import { FaPlus } from "react-icons/fa6";
 import Sidebar from "@/app/components/Sidebar";
 import { DataTable } from "./data-table";
+import Loading from "@/app/components/Loading";
 
 let success = null;
 let failed = null;
@@ -12,7 +13,6 @@ let failed = null;
 const Satuan = async () => {
   const [data, setData] = useState<any[]>([]);
   const [filter, setFilter] = useState("name");
-
 
   const getSatuans = async () => {
     const res = await fetch(process.env.APP_URL + `/api/satuan`);
@@ -24,29 +24,31 @@ const Satuan = async () => {
   useEffect(() => {
     getSatuans();
   }, []);
-  
+
   return (
     <>
-      <Sidebar />
-      <section className="py-24 sm:ml-[16rem]">
-        <div className="container">
-          <div className="mb-2 flex gap-4 items-center justify-between">
-            <h1 className="text-3xl font-bold mb-2">Satuan</h1>
-            <a
-              href="/admin/satuan/add"
-              className="bg-blue-600 text-white px-2 py-1 font-semibold rounded-lg flex gap-2 items-center"
-            >
-              <FaPlus />
-              Tambah Satuan
-            </a>
-          </div>
-          <DataTable
-            columns={columns}
-            data={data}
-            filter={filter}
-          />
+      {data.length === 0 ? (
+        <Loading />
+      ) : (
+        <div>
+          <Sidebar />
+          <section className="py-24 sm:ml-[16rem]">
+            <div className="container">
+              <div className="mb-2 flex gap-4 items-center justify-between">
+                <h1 className="text-3xl font-bold mb-2">Satuan</h1>
+                <a
+                  href="/admin/satuan/add"
+                  className="bg-blue-600 text-white px-2 py-1 font-semibold rounded-lg flex gap-2 items-center"
+                >
+                  <FaPlus />
+                  Tambah Satuan
+                </a>
+              </div>
+              <DataTable columns={columns} data={data} filter={filter} />
+            </div>
+          </section>
         </div>
-      </section>
+      )}
     </>
   );
 };
