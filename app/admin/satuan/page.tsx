dafa77ece3e25126d4b733bbb688623f427a6d7f
wahyudwiprstka <wahyudwiprstka@gmail.com>
2024-08-16 16:37:10 +0800

    test commit

--- app/admin/satuan/page.tsx
+++ app/admin/satuan/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { columns } from "./columns";
 import { FaPlus } from "react-icons/fa6";
 import Sidebar from "@/app/components/Sidebar";
@@ -7,18 +9,22 @@ import { DataTable } from "./data-table";
 let success = null;
 let failed = null;
 
-const getSatuans = async () => {
-  const res = await fetch(process.env.APP_URL + `/api/satuan`);
-  const data = await res.json();
-  return data;
-};
-
 const Satuan = async () => {
-  const data = await getSatuans();
-  const link = "/admin/satuan";
-  const api = "/api/satuan";
-  const filter = "name";
-  const dataType = "satuan";
+  const [data, setData] = useState<any[]>([]);
+  const [filter, setFilter] = useState("name");
+
+
+  const getSatuans = async () => {
+    const res = await fetch(process.env.APP_URL + `/api/satuan`);
+    const resp = await res.json();
+
+    setData(resp);
+  };
+
+  useEffect(() => {
+    getSatuans();
+  }, []);
+  
   return (
     <>
       <Sidebar />
@@ -37,11 +43,6 @@ const Satuan = async () => {
           <DataTable
             columns={columns}
             data={data}
-            link={link}
-            api={api}
-            success={success}
-            failed={failed}
-            dataType={dataType}
             filter={filter}
           />
         </div>
